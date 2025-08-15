acd1e4eb4399bc19013e7c31f9c24d759a764ed5
younhoso <thdbsgh3443@kakao.com>
2025-08-15 16:05:23 +0900

    Feat: 설정 변경

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: "",
+  basePath: "/repo",
+  assetPrefix: "/repo",
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "via.placeholder.com", // 이미지 도메인 추가
+      },
+    ],
+  },
   webpack(config, { dev }) {
     if (dev) config.cache = { type: "memory" }; // PackFileCacheStrategy 우회
     config.module.rules.push(
@@ -21,15 +31,6 @@ const nextConfig = {
   compiler: {
     // ssr 및 displayName은 기본적으로 구성됩니다.
     styledComponents: true,
-  },
-  images: {
-    unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "via.placeholder.com", // 이미지 도메인 추가
-      },
-    ],
   },
 };
 
